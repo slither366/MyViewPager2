b322e9ef4de5a6309ea43f9a0fb6925ac2b97ee3
slither366 <slither366@gmail.com>
2020-04-28 20:15:59 -0500

    Subiendo ejemplo terminado de Uso de TabLayout con ViewPager2 y Badge Drawable

---
+++ app/src/main/java/com/example/myviewpager2/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.myviewpager2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager);
+        viewPager2.setAdapter(new OrdersPagerAdapter(this));
+
+        final TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+                tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0: {
+                        tab.setText("Pendientes");
+                        tab.setIcon(R.drawable.ic_pending);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(
+                                ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)
+                        );
+                        badgeDrawable.setVisible(true);
+                        break;
+                    }
+                    case 1: {
+                        tab.setText("Confirmados");
+                        tab.setIcon(R.drawable.ic_confirmed);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(
+                                ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)
+                        );
+                        badgeDrawable.setNumber(8);
+                        badgeDrawable.setVisible(true);
+                        break;
+                    }
+                    case 2: {
+                        tab.setText("Delivered");
+                        tab.setIcon(R.drawable.ic_delivered);
+                        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(
+                                ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)
+                        );
+                        badgeDrawable.setVisible(true);
+                        badgeDrawable.setNumber(100);
+                        badgeDrawable.setMaxCharacterCount(3);
+                        break;
+                    }
+
+                }
+            }
+        }
+        );
+        tabLayoutMediator.attach();
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                BadgeDrawable badgeDrawable = tabLayout.getTabAt(position).getOrCreateBadge();
+                badgeDrawable.setVisible(false);
+            }
+        });
+    }
+}
